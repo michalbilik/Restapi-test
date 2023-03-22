54b631f1fd8ac5bdf7e30260c5d34f9a59d0fe30
Michał Bilik <michal.bilik@amica.com.pl>
2023-03-22 15:48:24 +0100

    simple get and post request and response with middleware

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const PORT = 8080;
 
-app.listen(
-    PORT, 
-    () => console.log(`it's alive on http://localhost:${PORT}`)
-);
+app.use(express.json())
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "tshirt",
+    size: "large",
+  });
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!'})
+  }
+
+  res.send({
+    tshirt: `tshirt with your ${logo} and ID of ${id}`,
+  })
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
